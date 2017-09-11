4b97ac3c1ef789cc5381746dc7a0dfe1d69861ae
madsbyday <madsbyday@gmail.com>
2017-09-11 11:32:32 +0200

    tilføjet en tester til db

---
+++ CA1/src/main/java/tester/test.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tester;
+
+import java.util.HashMap;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author vfgya_000
+ */
+public class test {
+    public static void main(String[] args) {
+        HashMap puproperties = new HashMap();
+        
+        puproperties.put("javax.persistence.sql-load-script-source", "scripts/initdb.sql");
+        Persistence.generateSchema("CAPU", puproperties);
+        
+        puproperties.remove("javax.persistence.sql-load-script-source");
+        Persistence.generateSchema("CAPU", puproperties);
+    }
+    
+}
