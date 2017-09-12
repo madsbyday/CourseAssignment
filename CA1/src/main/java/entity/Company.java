42fd0dc674c9fb5a03e30b172200cd3a44373966
madsbyday <madsbyday@gmail.com>
2017-09-12 14:31:11 +0200

    Changes to entity classes

--- CA1/src/main/java/entity/Company.java
+++ CA1/src/main/java/entity/Company.java
@@ -19,12 +19,6 @@ import javax.persistence.InheritanceType;
  */
 @Entity
 public class Company extends InfoEntity implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
     
     private String name;
     
@@ -35,14 +29,6 @@ public class Company extends InfoEntity implements Serializable {
     private int numEmployees;
     
     private double marketValue;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
@@ -84,31 +70,5 @@ public class Company extends InfoEntity implements Serializable {
         this.marketValue = marketValue;
     }
 
-    
-    
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Company)) {
-            return false;
-        }
-        Company other = (Company) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "entity.Company[ id=" + id + " ]";
-    }
     
 }
--- CA1/src/main/java/entity/Person.java
+++ CA1/src/main/java/entity/Person.java
@@ -22,23 +22,11 @@ public class Person extends InfoEntity implements Serializable {
 
     @ManyToMany(mappedBy = "persons")
     private List<Hobby> hobbys;
-
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
     
     private String firstName;
     
     private String lastName;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public List<Hobby> getHobbys() {
         return hobbys;
@@ -65,30 +53,5 @@ public class Person extends InfoEntity implements Serializable {
     }
     
     
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Person)) {
-            return false;
-        }
-        Person other = (Person) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "entity.Person[ id=" + id + " ]";
-    }
     
 }
